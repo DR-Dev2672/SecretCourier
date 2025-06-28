796eb179b6aa87a8031d7388220a6e922be644b7
dev <thakurdev2672@gmail.com>
2025-06-29 03:45:16 +0530

    resend email verification part

---
+++ src/helper/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/verificationEmail";
+
+export async function sendVerificationEmail(email:string,username:string,verificationCode:string):Promise<ApiResponse>{
+ 
+    try {
+    await resend.emails.send({
+    from: 'dev@drd.com',
+    to: email,
+    subject: 'Secret Courier Verification Code',
+    react: VerificationEmail({username,otp:verificationCode}),
+
+    
+});
+return {success:true,message:"Verification email sent successfully"}
+        
+
+    } catch (error) {
+        console.log("Error sending verification email",error)
+        return {success:false,message:"Failed to send verification email"}
+    }
+}
