2230dcb5550312e6ca1290afacde35812400ffb9
dev <thakurdev2672@gmail.com>
2025-07-01 17:53:19 +0530

    resend mail from change

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -6,7 +6,7 @@ export async function sendVerificationEmail(email:string,username:string,verific
  
     try {
     await resend.emails.send({
-    from: 'dev@drd.com',
+    from: 'Acme <onboarding@resend.dev>',
     to: email,
     subject: 'Secret Courier Verification Code',
     react: VerificationEmail({username,otp:verificationCode}),
