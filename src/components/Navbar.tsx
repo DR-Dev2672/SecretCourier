743bfd0aca414642f0f6f8cb5c6995c890aed09a
dev <thakurdev2672@gmail.com>
2025-07-19 20:45:40 +0530

    debugging in existing filess

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useSession ,signOut} from 'next-auth/react'
 import { User } from 'next-auth';
-import { Button } from '@react-email/components';
+import {Button} from '@/components/ui/button';
 import Link from 'next/link';
 
 
@@ -12,18 +12,18 @@ function Navbar() {
   return (
    
    <>
-   <nav className='className="p-4 md:p-6 shadow-md bg-gray-900 text-white"'>
+   <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
     <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">True Feedback</a>
         {session?
         (
             <>
-            <span>Welcome,{user.username||user.email}</span>
-            <Button  onClick={()=>signOut} className="w-full md:w-auto bg-slate-100 text-black" >Logout</Button>
+            <span className="mr-4">Welcome,{user.username||user.email}</span>
+            <Button  onClick={()=>signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline' >Logout</Button>
             </>
         ):(
             <Link href="/sign-in">
-                <Button className="w-full md:w-auto bg-slate-100 text-black">Login</Button>
+                <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
             </Link>
 
         )
--- src/schema/messageSchema.ts
+++ src/schema/messageSchema.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod';
-export const MessageSchema=z.object({
+export const messageSchema=z.object({
     content:z.string()
     .min(10, 'Message must be at least 10 characters long')
     .max(300, 'Message must be at most 300 characters long'),
